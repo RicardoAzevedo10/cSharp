3966f396466911975cfb01425f4c6bcd68aff2c0
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-10 13:25:18 +0000

    Add files via upload

---
+++ BattleShip/BattleShip/BattleshipBoard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class BattleshipBoard
+    {
+        public void DisplayBoard(char[,] Board)
+        {
+            int Row;
+            int Column;
+
+            Console.WriteLine("  ¦ 0 1 2 3 4 5 6 7 8 9");
+            Console.WriteLine("--+--------------------");
+            for (Row = 0; Row <= 9; Row++)
+            {
+                Console.Write((Row).ToString() + " ¦ ");
+                for (Column = 0; Column <= 9; Column++)
+                {
+                    Console.Write(Board[Column, Row] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
