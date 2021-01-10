4651a23e7b2c1c16c389c06ce89eaa56deaaca43
Ricardo Azevedo <47283892+RicardoAzevedo10@users.noreply.github.com>
2021-01-10 13:24:44 +0000

    Add files via upload

---
+++ ClientServer/ClientServer/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteNetLib;
+
+namespace ClientServer
+{
+    public class Client
+    {
+        NetManager netmanager;
+        EventBasedNetListener netListener;
+
+
+        void start()
+        {
+            netListener = new EventBasedNetListener();
+            netListener.PeerConnectedEvent += (server) =>
+            {
+                Debug.log();
+            }
+        }
+
+        void update()
+        {
+
+        }
+
+    }
+}
